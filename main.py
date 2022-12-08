b2a5312a6cdd98ac9797bb593af6bb78ff7566ab
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2022-12-09 01:02:53 +0300

    Review

--- main.py
+++ main.py
@@ -1,53 +1,68 @@
 import datetime
 import pandas
 import collections
+import argparse
+from pprint import pprint
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def make_wine_dict():
-    wine_data = pandas.read_excel("wine.xlsx", sheet_name="Лист1",
+def collect_input_data(file_path="wine.xlsx"):
+    input_data = pandas.read_excel(file_path, sheet_name="Лист1",
                                   na_values=['N/A', 'NA'],
                                   keep_default_na=False)
-    wine_data = wine_data.to_dict(orient='record')
-    product_data = collections.defaultdict(list)
-    for wine in wine_data:
+    input_data = input_data.to_dict(orient='record')
+    wine_info = collections.defaultdict(list)
+    for wine in input_data:
         category = wine["Категория"]
-        product_data[category].append(wine)
-    return product_data
+        wine_info[category].append(wine)
+    return wine_info
 
 
-def spelling_year(age):
+def get_correct_spelling(age):
     year = "лет"
-    if (age // 10) % 10 != 1:
-        if age % 10 == 1:
-            year = "год"
-        elif age % 10 in (2, 3, 4):
-            year = "года"
+    if age % 10 == 1:
+        year = "год"
+    if age % 10 in (2, 3, 4):
+        year = "года"
+    if (age//10) % 10 == 1:
+        year = "лет"
     return year
 
 
-establishment_date = datetime.datetime(year=1922, month=1, day=1)
-current_date = datetime.datetime.now()
-winery_age = current_date - establishment_date
-winery_age = winery_age.days//365
-spelling_years = ""
-splelling_years = str(spelling_year(winery_age))
-wine_data = dict(make_wine_dict())
+def parse_arg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--directory', nargs='?',
+                        help='Path to input data file',
+                        default="wine.xlsx")
+    arg = parser.parse_args()
+    return arg
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-template = env.get_template('template.html')
-rendered_page = template.render(
-    wines=wine_data,
-    establishment_date=winery_age,
-    spelled_year=splelling_years
-)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def main():
+    arg = parse_arg()
+    file_path = arg.directory
+    establishment_year = 1922
+    current_date = datetime.datetime.now()
+    winery_age = current_date.year - establishment_year
+    splelling_years = get_correct_spelling(winery_age)
+    wine_info = collect_input_data(file_path)
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        wines=wine_info,
+        establishment_date=winery_age,
+        spelled_year=splelling_years
+    )
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+    
+    
+if __name__ == "__main__":
+    main()
